820c36c561b6649cdb29d0e08ad5463fc34b5c7b
ahoodiakov <akhudiakov97@gmail.com>
2023-09-19 08:49:59 +0100

    static-website

--- components/wx-client/WeatherProvider.tsx
+++ components/wx-client/WeatherProvider.tsx
@@ -19,20 +19,43 @@ export default function WeatherProvider({children}: ReactChildren) {
   const [unit, setUnit] = useState('imperial')
   const [weather, setWeather] = useState({})
 
-  // Fetch the weather data on mount.
+// PLEASE USE YOUR OWN API KEYS !!!
   useEffect(() => {
-    async function searchWeather() {
+    const fetchWeather = async () => {
+      const location = encodeURI('Your Location') // replace 'Your Location' with your actual location
+      let lat = 28.3886186
+      let lng = -81.5659069
+
       try {
-        const response = await fetch(`/api/weather?location=${location}`)
-        const data = await response.json()
-        setWeather(data)
+        const geocodeResponse = await fetch(
+          `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_MAPS_API_KEY}`
+        )
+        const geocodeData = await geocodeResponse.json()
+
+        if (geocodeData.status === 'OK' && geocodeData.results.length > 0) {
+          lat = geocodeData.results[0].geometry.location.lat
+          lng = geocodeData.results[0].geometry.location.lng
+        }
+      } catch (error) {
+        console.error(error)
+      }
+
+      try {
+        const weatherResponse = await fetch(
+          `https://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHERAPI_KEY}&q=${lat},${lng}`
+        )
+        const weatherData = await weatherResponse.json()
+
+        if (weatherData.location) {
+          setWeather(weatherData)
+        }
       } catch (error) {
         console.error(error)
       }
     }
 
-    searchWeather()
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+    fetchWeather()
+  }, [])
 
   return (
     <WeatherContext.Provider
